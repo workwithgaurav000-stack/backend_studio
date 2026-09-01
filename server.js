02de28efd3872e61d0c49c07339732f84eae3bb1
workwithgaurav000 <workwithgaurav000@gmail.com>
2026-09-01 18:15:20 +0530

    Update Render backend URL references

--- server.js
+++ server.js
@@ -162,7 +162,7 @@ async function startServer() {
         console.log("✅ MongoDB Connected Successfully!");
 
         app.listen(PORT, () => {
-            console.log(`🚀 Server running at https://studio-gaurav-1.onrender.com:`);
+            console.log(`🚀 Server running at https://backend-studio-wv1k.onrender.com:`);
         });
 
     } catch (error) {
